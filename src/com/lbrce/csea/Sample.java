4e6f1a0a4e9aa01c477ef56e3041ce027e1cc2dc
AdmiN <AdmiN@ITWS11>
2023-09-21 14:02:15 +0530

    team lead commit.

---
+++ src/com/lbrce/csea/Sample.java
@@ -0,0 +1,15 @@
+package com.lbrce.csea;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mahesh();
+
+	}
+	public static void mahesh() {
+		System.out.println("this is team leader");
+		
+			}
+
+}
