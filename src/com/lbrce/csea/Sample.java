038e0795ac3ff8b6bc3b62a25810effce2e5a533
AdmiN <AdmiN@ITWS09>
2023-09-21 14:30:51 +0530

    santhu commit

--- src/com/lbrce/csea/Sample.java
+++ src/com/lbrce/csea/Sample.java
@@ -5,10 +5,14 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		mahesh();
-
+        santhu();
 	}
 	public static void mahesh() {
 		System.out.println("this is team leader");
+		
+			}
+	public static void santhu() {
+		System.out.println("this is team member");
 		
 			}
 
